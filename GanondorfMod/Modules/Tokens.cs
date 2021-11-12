8cb810957af7bc4838850e411394d0ee659e1f35
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2021-11-13 03:21:52 +1100

    Updating Tokens

---
+++ GanondorfMod/Modules/Tokens.cs
@@ -0,0 +1,67 @@
+﻿using R2API;
+using System;
+
+namespace GanondorfMod.Modules
+{
+    internal static class Tokens
+    {
+        internal static void AddTokens()
+        {
+            #region Henry
+            string prefix = GanondorfPlugin.developerPrefix + "_HENRY_BODY_";
+
+            string desc = "Ganondorf, the Great King of Evil has arrived! <color=#CCD3E0>" + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Lay down a simple punch and send foes flying, or dash through enemies!" + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Use Flame Choke to grab multiple enemies in a row!" + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Wizard's Foot sends you hurling through enemies!" + Environment.NewLine + Environment.NewLine;
+            desc = desc + "< ! > Soak up damage and retaliate with Warlock Punch!" + Environment.NewLine + Environment.NewLine;
+
+            string outro = "..and so he left, spreading malice in his wake.";
+            string outroFailure = "..and so he vanished, looking for stronger foes.";
+
+            LanguageAPI.Add(prefix + "NAME", "Ganondorf");
+            LanguageAPI.Add(prefix + "DESCRIPTION", desc);
+            LanguageAPI.Add(prefix + "SUBTITLE", "The Great King of Evil");
+            LanguageAPI.Add(prefix + "LORE", "??");
+            LanguageAPI.Add(prefix + "OUTRO_FLAVOR", outro);
+            LanguageAPI.Add(prefix + "OUTRO_FAILURE", outroFailure);
+
+            #region Skins
+            LanguageAPI.Add(prefix + "DEFAULT_SKIN_NAME", "Default");
+            LanguageAPI.Add(prefix + "MASTERY_SKIN_NAME", "Alternate");
+            #endregion
+
+            #region Passive
+            LanguageAPI.Add(prefix + "PASSIVE_NAME", "Henry passive");
+            LanguageAPI.Add(prefix + "PASSIVE_DESCRIPTION", "Sample text.");
+            #endregion
+
+            #region Primary
+            LanguageAPI.Add(prefix + "PRIMARY_SLASH_NAME", "Sword");
+            LanguageAPI.Add(prefix + "PRIMARY_SLASH_DESCRIPTION", Helpers.agilePrefix + $"Swing forward for <style=cIsDamage>{100f * StaticValues.swordDamageCoefficient}% damage</style>.");
+            #endregion
+
+            #region Secondary
+            LanguageAPI.Add(prefix + "SECONDARY_GUN_NAME", "Handgun");
+            LanguageAPI.Add(prefix + "SECONDARY_GUN_DESCRIPTION", Helpers.agilePrefix + $"Fire a handgun for <style=cIsDamage>{100f * StaticValues.gunDamageCoefficient}% damage</style>.");
+            #endregion
+
+            #region Utility
+            LanguageAPI.Add(prefix + "UTILITY_ROLL_NAME", "Roll");
+            LanguageAPI.Add(prefix + "UTILITY_ROLL_DESCRIPTION", "Roll a short distance, gaining <style=cIsUtility>300 armor</style>. <style=cIsUtility>You cannot be hit during the roll.</style>");
+            #endregion
+
+            #region Special
+            LanguageAPI.Add(prefix + "SPECIAL_BOMB_NAME", "Bomb");
+            LanguageAPI.Add(prefix + "SPECIAL_BOMB_DESCRIPTION", $"Throw a bomb for <style=cIsDamage>{100f * StaticValues.bombDamageCoefficient}% damage</style>.");
+            #endregion
+
+            #region Achievements
+            LanguageAPI.Add(prefix + "MASTERYUNLOCKABLE_ACHIEVEMENT_NAME", "Henry: Mastery");
+            LanguageAPI.Add(prefix + "MASTERYUNLOCKABLE_ACHIEVEMENT_DESC", "As Henry, beat the game or obliterate on Monsoon.");
+            LanguageAPI.Add(prefix + "MASTERYUNLOCKABLE_UNLOCKABLE_NAME", "Henry: Mastery");
+            #endregion
+            #endregion
+        }
+    }
+}
