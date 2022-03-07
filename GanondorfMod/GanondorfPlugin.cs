f579a36becec28fbfe052303ea777c7a7d321475
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2022-03-08 01:54:43 +1100

    Update GanondorfPlugin.cs

--- GanondorfMod/GanondorfPlugin.cs
+++ GanondorfMod/GanondorfPlugin.cs
@@ -32,7 +32,7 @@ namespace GanondorfMod
     {
         public const string MODUID = "com.Ethanol10.Ganondorf";
         public const string MODNAME = "Ganondorf";
-        public const string MODVERSION = "2.1.0";
+        public const string MODVERSION = "2.1.1";
         
         //Triforce Buff
         public static TriforceBuffComponent triforceBuff;
